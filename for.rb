9b1ffcd08e5694f6bbb22275f6fb65b9074947e6
Joan Manuel Araujo <WISDOMBTHEGAME@GMAIL.COM>
2016-01-29 11:11:03 +0000

    Done.

--- for.rb
+++ for.rb
@@ -1,7 +1,20 @@
 
 def using_for
-	checklist = 1..10
-	#your code here
+  for checklist in 1..10 do
+    puts "Wingardium Leviosa"        #your code here
+  end
 end
+checklist = 1..10
+#for current_iteration_number in 1..100 do
+   #puts "Hello world, this is number #{current_iteration_number}"
+#end
 
 
+#for i in 0..5
+ #  puts "Value of local variable is #{i}"
+#end
+  #shoe_count = 1..32
+
+#for shoe in shoe_count
+   # puts "I'm putting a pair of shoes away."
+#end
